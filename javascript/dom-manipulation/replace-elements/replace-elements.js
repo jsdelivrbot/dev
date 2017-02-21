915550d3be82e2f0a8c73f8d2fea496e58d33d6b
Ben Chung <ben@benchung.com>
2017-02-21 16:46:58 -0500

    updated js

---
+++ javascript/dom-manipulation/replace-elements/replace-elements.js
@@ -0,0 +1,6 @@
+var parent = document.getElementById("div1");
+var child = document.getElementById("p1");
+var para = document.createElement("p");
+var node = document.createTextNode("This is new.");
+para.appendChild(node);
+parent.replaceChild(para,child);
