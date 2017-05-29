ad6e108874053dace821d62c109026e806d80011
Ben Chung <ben@benchung.com>
2017-05-29 17:18:15 -0400

    update pwa info

---
+++ flighplan/flightplan.js
@@ -0,0 +1,11 @@
+//flightplan docs:
+//https://gist.github.com/learncodeacademy/393c8ec3ebaa92df9a29
+
+//video
+//https://www.youtube.com/watch?v=vmekjPhQuCc
+
+//simple deployment:
+//https://gist.github.com/learncodeacademy/393c8ec3ebaa92df9a29
+
+//deployment with node:
+//https://gist.github.com/learncodeacademy/35045e64d2bbe6eb14f9
