1762b0aadc2df2d8c26e4367e6117f366a87883e
Ben Chung <ben@benchung.com>
2017-11-08 12:46:55 -0500

    applying stash

--- javascript/array-manipulation/arrray-manipulation.js
+++ javascript/array-manipulation/arrray-manipulation.js
@@ -53,6 +53,32 @@ console.log(fruits);
 fruits[fruits.length] = "Grape";
 console.log(fruits);
 
+//Sort
+//---------------------------------------
+
+// arr.sort()
+var scores = [1, 10, 21, 2]; 
+scores.sort(); // [1, 10, 2, 21]
+// Note that 10 comes before 2,
+// because '10' is mix of two characters '1' and '0' so '10' is before '2' in Unicode code point order.
+
+// If compareFunction is supplied, the array elements are sorted according to the return value of the compare function. If a and b are two elements being compared, then:
+
+// If compareFunction(a, b) is less than 0, sort a to an index lower than b, i.e. a comes first.
+// If compareFunction(a, b) returns 0, leave a and b unchanged with respect to each other, but sorted with respect to all different elements. Note: the ECMAscript standard does not guarantee this behaviour, and thus not all browsers (e.g. Mozilla versions dating back to at least 2003) respect this.
+// If compareFunction(a, b) is greater than 0, sort b to a lower index than a.
+// compareFunction(a, b) must always return the same value when given a specific pair of elements a and b as its two arguments. If inconsistent results are returned then the sort order is undefined.
+
+// arr.sort(compareFunction)
+var numbers = [4, 2, 5, 1, 3];
+numbers.sort(function(a, b) {
+  return a - b;
+});
+console.log(numbers);
+
+// [1, 2, 3, 4, 5]
+
+
 //Splice
 //---------------------------------------
 
