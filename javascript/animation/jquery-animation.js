aab80115a7a0f2104d3b398b904ea06b19ffc046
Ben Chung <ben@benchung.com>
2017-11-16 08:57:39 -0500

    updated

--- javascript/animation/jquery-animation.js
+++ javascript/animation/jquery-animation.js
@@ -2,9 +2,26 @@
 //normally don't use fadeIn/fadeOut
 $('.loading-container').fadeTo(500, 1);
 $('.loading-container').fadeTo(500, 0.5);
-//fadeto callback example
-$( "#book" ).fadeTo( "slow" , 0.5, function() {
+
+
+/* ==========================================================================
+//fadeto callback with visibility example
+========================================================================== */
+
+//fade in
+$(this).css("visibility", "visible"); 
+$( "#book" ).fadeTo( "slow" , 1, function() {
 // Animation complete.
 });
+
+//fade out
+$( "#book" ).fadeTo( "slow" , 0, function() {
+	$(this).css("visibility", "hidden"); 
+// Animation complete.
+});
+
+/* ==========================================================================
 //or for immediate results
+========================================================================== */
+
 jQuery('#main').css('opacity', '0.6');
