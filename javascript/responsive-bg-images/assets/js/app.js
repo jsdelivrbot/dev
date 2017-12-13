b8c8a08e41a8ae045b8e08582ecec55cf7180f57
Ben Chung <ben@benchung.com>
2017-12-13 17:16:47 -0500

    added responsive images js

---
+++ javascript/responsive-bg-images/assets/js/app.js
@@ -0,0 +1,53 @@
+(function() {
+	/* ==========================================================================
+	variables
+	========================================================================== */
+	var mediaSize = '';
+	var mobileThreshold = 480;
+	var tabletThreshold = 768;
+	var responsiveImages = [].slice.call(document.querySelectorAll('.responsive-image'));
+
+	/* ==========================================================================
+	window size
+	========================================================================== */
+
+	//if mobile detexted or smaller screen than 600
+	checkMobile();
+
+	//check window size (jquery)
+	$(window).resize(function() {
+		checkMobile();
+	});
+
+	function checkMobile() {
+		if(window.innerWidth < mobileThreshold) {
+			if(mediaSize !== 'mobile') {
+				responsiveBgImages('small');
+			}
+		} else if(window.innerWidth >= mobileThreshold && window.innerWidth < tabletThreshold) {
+			if(mediaSize !== 'tablet') {
+				responsiveBgImages('medium');
+			}
+		}
+		else {
+			if(mediaSize !== 'desktop') {
+				responsiveBgImages('large');
+			}
+		}
+	}
+
+
+
+	/* ==========================================================================
+	// responsive images
+	========================================================================== */
+
+	function responsiveBgImages(size) {
+		console.log('size: ', size)
+		for(var i = 0; i < responsiveImages.length; i++) {
+			var urlString = 'url(' + responsiveImages[i].dataset[size] + ')';
+			responsiveImages[i].style.backgroundImage = urlString;
+		}
+	}
+
+})();
