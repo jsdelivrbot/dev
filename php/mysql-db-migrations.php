d48cb87b45f7629f842c3b411839bfd3e0ebfd41
Ben Chung <ben@benchung.com>
2018-07-26 12:05:25 -0400

    Create mysql-db-migrations.php

---
+++ php/mysql-db-migrations.php
@@ -0,0 +1,111 @@
+/* ==========================================================================
+database migrations
+========================================================================== */
+
+
+/* ==========================================================================
+create column
+========================================================================== */
+
+public function create_column() {
+	$table = 'table_name';
+	$new_column = 'column_name';
+	if (! $this->db->field_exists($new_column, $table)) {
+		$this->db->query('ALTER TABLE '.$table.' ADD COLUMN '.$new_column.' datetime NULL');
+
+		echo '<p>'.$table.' columns: '.$new_column.' created</p>';
+	} else {
+		echo '<p>'.$table.' columns: '.$new_column.' already exist</p>';
+	}
+}
+
+/* ==========================================================================
+create table
+========================================================================== */
+
+public function create_table() {
+	$table = 'table_name';
+	if (! $this->db->table_exists($table)) {
+		$this->db->query(
+			'CREATE TABLE IF NOT EXISTS '.$table.' (
+				`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+				`foreign_id` INT(10) UNSIGNED NOT NULL,
+				`name` varchar(100) DEFAULT NULL,
+				`ref_type` enum("option_one","option_two") NOT NULL,
+				`true_or_false` INT(1) UNSIGNED NULL DEFAULT 0,
+				`created_at` DATETIME NULL,
+				`deleted_at` DATETIME NULL,
+				PRIMARY KEY (`id`)
+			)'
+		);
+		echo '<p>'.$table.' table created<p/>';
+	} else {
+		echo '<p>'.$table.' table already exist</p>';
+	}
+}
+
+/* ==========================================================================
+create a join table
+========================================================================== */
+
+public function create_join_table() {
+	$table = 'table_name';
+	if (! $this->db->table_exists($table)) {
+		$this->db->query('
+			CREATE TABLE
+			IF NOT EXISTS '.$table.' (
+				`apples_id` INT UNSIGNED NOT NULL,
+				`oranges_id` INT UNSIGNED NOT NULL,
+				PRIMARY KEY (
+					`apples_id`,
+					`oranges_id`
+				)
+			)
+		');
+
+		echo '<p>'.$table.' table created</p>';		
+	} else {
+		echo '<p>'.$table.' table already exist</p>';
+	}
+}
+
+/* ==========================================================================
+insert or update row
+========================================================================== */
+
+$q = $this->db->from('users')->where('id',$id)->get();
+
+$data = array(
+        'title' => 'My title',
+        'name' => 'My Name',
+        'date' => 'My date'
+);
+
+if ( $q->num_rows() > 0 ) {
+	//update
+  	$this->db->where('id',$id)->update('my_table',$data);
+} else {
+	//insert
+ 	$this->db->insert('my_table', $data);
+}
+
+
+/* ==========================================================================
+add another enum to existing col
+========================================================================== */
+
+public function add_enum_type()
+{
+	$table = 'my_table';
+	$column = 'culumn_to_alter';
+	$new_enum_types = '"type1","type2","type3"';
+	$default_type = '"type1"';
+
+	$result = $this->db->query('SHOW COLUMNS FROM '.$table.' LIKE "'.$column.'"')->result();
+	if (! ($result[0]->Type === 'enum('.$new_enum_types.')')) {
+		$this->db->query('ALTER TABLE '.$table.' CHANGE '.$column.' '.$column.' ENUM('.$new_enum_types.') DEFAULT '.$default_type);
+		echo '<p>+ new enum types: '.$new_enum_types.' add for table '.$table.'</p>';
+	} else {
+		echo '<p># enum types '.$new_enum_types.' for table '.$table.' already exist</p>';
+	}
+}
