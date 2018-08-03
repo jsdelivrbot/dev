c8548e174b9f5a6654dbb0e0101220a59a24c80e
Ben Chung <ben@benchung.com>
2018-08-03 15:44:25 -0400

    Create eval.php

---
+++ php/eval.php
@@ -0,0 +1,17 @@
+//Evaluates the given code as PHP.
+// The code will be executed in the scope of the code calling eval(). 
+// Thus any variables defined or changed in the eval() call will remain visible after it terminates.
+// don't put <?php ?> tags in it.
+
+$string = "beautiful";
+$time = "winter";
+
+$str = 'This is a $string $time morning!';
+echo $str. "<br>";
+
+eval("\$str = \"$str\";");
+echo $str;
+
+// prints:
+// This is a $string $time morning!
+// This is a beautiful winter morning!
